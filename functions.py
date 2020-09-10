5241341ee2116e921c2d7e41df4878928eda1fc7
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-09-09 22:31:57 -0700

    finished
    I have finished my assignment

--- functions.py
+++ functions.py
@@ -12,3 +12,16 @@ print(result)
 def subtract(num1, num2):
     answer = num1 - num2
     return answer
+answer = subtract(5, 2)
+print(answer)
+answer = subtract(20,15)
+print(answer)
+
+
+def add(int1, int2):
+    ansr = int1 + int2
+    return ansr
+ansr = add(5, 6)
+print(ansr)
+ansr = add(20,10)
+print(ansr)
