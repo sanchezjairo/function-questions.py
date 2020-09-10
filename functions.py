53479e3f5e27c47adce4cd30d5f921ca610c6b3c
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-09-09 22:21:21 -0700

    progress
    I have added question 3

--- functions.py
+++ functions.py
@@ -3,6 +3,10 @@ def area(width, height):
     return result
 result = area(5, 6)
 print(result)
+result = area(5, 3)
+print(result)
+result = area(20, 3)
+print(result)
 
 
 def subtract(num1, num2):
