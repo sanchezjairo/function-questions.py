169bcfcaca2928038a40c5870425eaa37f02f584
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-09-09 22:19:03 -0700

    progress
    so far i have done 4 questions lets go!!

--- functions.py
+++ functions.py
@@ -1,1 +1,10 @@
+def area(width, height):
+    result = width * height
+    return result
+result = area(5, 6)
+print(result)
 
+
+def subtract(num1, num2):
+    answer = num1 - num2
+    return answer
